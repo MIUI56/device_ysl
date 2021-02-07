ff20343fa8406cbdabaf2995e8beae21d18cf1ed
MIUI56 <payush706@gmail.com>
2021-02-07 17:22:08 +0000

    Revert "ysl: Set TARGET_COPY_OUT_ For System_ext"
    This reverts commit 51bff0564e6863f6113e903a50f2072b57803c0d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,8 +130,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
     /mnt/vendor/persist:/persist
-TARGET_COPY_OUT_VENDOR := vendor	
-TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
+TARGET_COPY_OUT_VENDOR := vendor
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # FM
