5752727d040cdf56921722eb615c371fa8d6e203
MIUI56 <payush706@gmail.com>
2021-02-27 11:28:23 +0000

    ysl:Use phase offsets as duration In Android R, the SurfaceFlinger calculate phase offsets based on duration. Adopt the new way of specifying Signed-off-by: MIUI56 <payush706@gmail.com> phase offsets to SurfaceFlinger. Bug: 158464992# Test: scrolling, app launching/closing, playing video# On branch lineage-18.1 Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415# Your branch is ahead of 'origin/lineage-18.1' by 2 commits.

--- prop.mk
+++ prop.mk
@@ -264,7 +264,18 @@ persist.rcs.supported=1
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true
-    
+  
+debug.sf.use_phase_offsets_as_durations=1
+debug.sf.early_app_phase_offset_ns=500000
+debug.sf.late.sf.duration=10500000
+debug.sf.early_gl_phase_offset_ns=3000000
+debug.sf.late.app.duration=20500000
+debug.sf.early_gl_app_phase_offset_ns=15000000
+debug.sf.early.sf.duration=21000000
+debug.sf.early.app.duration=16500000
+debug.sf.earlyGl.sf.duration=13500000
+debug.sf.earlyGl.app.duration=21000000
+  
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true \
